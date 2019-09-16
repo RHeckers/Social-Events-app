13835855eca8e81c6453db8881ff6f327e617e6d
wolfyr <35544104+wolfyr@users.noreply.github.com>
2019-09-16 19:06:44 +0200

    attend, unattend and host event, client

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -19,7 +19,7 @@ const HomePage = () => {
             alt='logo'
             style={{ marginBottom: 12 }}
           />
-          SocialEvents
+          Reactivities
         </Header>
         {isLoggedIn && user ? (
           <Fragment>
